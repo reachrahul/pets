17f5b271417e6dcd4324cc3db86f923041f36612
rahul <reachrahulsaini@gmail.com>
2019-08-21 18:55:42 +0530

    updated all the animal's details

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/Cats">Cats </router-link>|
+      <router-link to="/Dogs">Dogs </router-link>|
     </div>
+    <b-container>
     <router-view/>
+    </b-container>
   </div>
 </template>
 
